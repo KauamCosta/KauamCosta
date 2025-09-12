50604399c0c22ce1ca042e0469d4e128599d9129
Kauam Costa <kauamcosta9@gmail.com>
2025-09-12 05:25:50 -0300

    Refactor UserProfile component and add theme toggle functionality; update index.html and styles for dark mode support

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,7 @@ import './index.css'
 import UserProfile from './UserProfile.tsx'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode >
-    <div className="flex h-screen w-screen justify-center">
-      <UserProfile />
-    </div>
+  <StrictMode>
+    <UserProfile />
   </StrictMode>,
 )
